943f9c6cdd0e9af77871ff699b98cbad4dc75a56
azX-45 <wazxwooz@gmail.com>
2020-11-12 16:55:56 +0100

    Ajout de fixtures Users

--- Blog/src/DataFixtures/AppFixtures.php
+++ Blog/src/DataFixtures/AppFixtures.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\DataFixtures;
-
+use\App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
@@ -11,7 +12,15 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-
+        $generator = Faker\Factory::create("fr_FR");
+        for($i =0 ; $i < 20 ; $i++){
+          $user = new User();
+          $user->setFirstName($generator->firstName())
+            ->setlastName($generator->lastName)
+            ->setEmail($generator->email)
+            ->setPassword('password');
+          $manager->persist($user);       
+        }
         $manager->flush();
     }
 }
