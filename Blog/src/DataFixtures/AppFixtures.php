9e6479810056f7b3e2f05d5e894179f0e763895e
azX-45 <wazxwooz@gmail.com>
2020-11-12 17:01:56 +0100

    mise en place de l'encodeur password symfony

--- Blog/src/DataFixtures/AppFixtures.php
+++ Blog/src/DataFixtures/AppFixtures.php
@@ -5,9 +5,15 @@ use\App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class AppFixtures extends Fixture
-{
+{   
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+      $this->encoder =$encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -15,10 +21,11 @@ class AppFixtures extends Fixture
         $generator = Faker\Factory::create("fr_FR");
         for($i =0 ; $i < 20 ; $i++){
           $user = new User();
+          $password =$this->encoder->encodePassword($user , 'password');
           $user->setFirstName($generator->firstName())
             ->setlastName($generator->lastName)
             ->setEmail($generator->email)
-            ->setPassword('password');
+            ->setPassword($password);
           $manager->persist($user);       
         }
         $manager->flush();
