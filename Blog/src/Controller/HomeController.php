86a432d11c6878efa5463bea2d87c1d120a54e0b
azX-45 <wazxwooz@gmail.com>
2020-11-12 13:12:02 +0100

    Migration table Post sur php Admin

--- Blog/src/Controller/HomeController.php
+++ Blog/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,17 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+
+        $post = new Post();
+        $post->setTitle('Titre 1')
+        ->setContent('salut')
+        ->setAuthor('julien')
+        ->setCreatedAt(new \DateTime());
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'Julien',
+            "post" => $post
         ]);
     }
 }
