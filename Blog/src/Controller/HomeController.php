d348a89e1038060b1a8a5640ed99fbb9fd1880d2
azX-45 <wazxwooz@gmail.com>
2020-11-12 18:13:00 +0100

    ajout public function show pour les posts + post html twig

--- Blog/src/Controller/HomeController.php
+++ Blog/src/Controller/HomeController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,14 +12,22 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(PostRepository $ripo)
     {
-
-        
+        $posts = $ripo->findAll();
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/posts/{id}", name="show_post")
+     */
+    public function show(Post $post)
+    {
+        return $this->render('home/post.html.twig', [
+            'post' => $post
         ]);
     }
 }
