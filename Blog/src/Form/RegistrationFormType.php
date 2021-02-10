4651620501c75d8d275cfd318dbaf1cad4d32deb
azX-45 <wazxwooz@gmail.com>
2021-02-10 15:01:02 +0100

    maj

--- Blog/src/Form/RegistrationFormType.php
+++ Blog/src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Email',
+                    'class' => 'form-control',
+                ],
+            ])
             ->add('lastName')
             ->add('firstName')
             ->add('agreeTerms', CheckboxType::class, [
