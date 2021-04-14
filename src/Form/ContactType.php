e4871b2aaabae9ed33d2788c983a340e28eb19b6
azX-45 <wazxwooz@gmail.com>
2021-04-14 15:00:45 +0200

    retrait d'un USE

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -48,7 +47,6 @@ class ContactType extends AbstractType
                     'class' => 'form-control',
                 ],
             ])
-
 
             ->add('sujet', TextType::class, [
                 'attr' => [
