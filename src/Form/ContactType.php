c898c88f9ac413b99169f312a403bb8b590ea771
azX-45 <wazxwooz@gmail.com>
2021-04-19 15:28:43 +0200

    ajout checkbox agreeterms

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,12 +3,14 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class ContactType extends AbstractType
 {
@@ -52,6 +54,15 @@ class ContactType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Sujet',
                     'class' => 'form-control',
+                ],
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
                 ],
             ])
 
